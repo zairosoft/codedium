205b4413a7eaf4514feff5a466396a7e60ee5622
Zairosoft <zairosoft@outlook.com>
2025-12-22 00:16:50 +0700

    d
    dd

--- modules/Expenses/routes/web.php
+++ modules/Expenses/routes/web.php
@@ -17,3 +17,15 @@ use Modules\Expenses\App\Http\Controllers\ExpensesController;
 Route::group([], function () {
     Route::resource('expenses', ExpensesController::class)->names('expenses');
 });
+
+
+Route::prefix('expenses')->group(function () {
+    Route::get('/', 'ExpensesController@index')->name('expenses');
+    Route::get('/report', 'ExpensesController@report')->name('expenses.report');
+    Route::get('/report/export', 'ExpensesController@exportReport')->name('expenses.report.export');
+    Route::get('/create', 'ExpensesController@create')->name('expenses.create');
+    Route::post('/store', 'ExpensesController@store')->name('expenses.store');
+    Route::get('/{id}/edit', 'ExpensesController@edit')->name('expenses.edit');
+    Route::put('/{id}/update', 'ExpensesController@update')->name('expenses.update');
+    Route::get('/{id}/show', 'ExpensesController@show')->name('expenses.show');
+});
