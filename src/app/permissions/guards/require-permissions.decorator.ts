7e184156898b6086ff883a31c256c2189e34ac1b
Natt <nakhonsoft@gmail.com>
2026-04-12 00:37:38 +0700

    [Fix]

--- src/app/permissions/guards/require-permissions.decorator.ts
+++
@@ -1,6 +0,0 @@
-import { SetMetadata } from '@nestjs/common';
-
-export const REQUIRED_PERMISSIONS_METADATA = 'required_permissions';
-
-export const RequirePermissions = (...permissions: string[]) =>
-  SetMetadata(REQUIRED_PERMISSIONS_METADATA, permissions);
