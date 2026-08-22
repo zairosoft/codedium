fb3f2c313a281052c16cd19a180d5286c31c6f03
Admin <zairosoft@outlook.com>
2026-08-23 04:09:54 +0700

    Update platform.module.ts

--- src/app/platform.module.ts
+++ src/app/platform.module.ts
@@ -4,11 +4,6 @@ import { ConfigService } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AUTH_SERVICE } from '../workless/interfaces/auth.interface';
-import { NOTIFICATION_SERVICE } from '../workless/interfaces/notification.interface';
-import { PERMISSION_SERVICE } from '../workless/interfaces/permission.interface';
-import { ROLE_SERVICE } from '../workless/interfaces/role.interface';
-import { USER_SERVICE } from '../workless/interfaces/user.interface';
 import { JwtAuthGuard } from '../workless/jwt/jwt-auth.guard';
 import { resolveJwtSecret } from '../config/jwt.config';
 import { JwtStrategy } from '../workless/jwt/jwt.strategy';
@@ -23,10 +18,10 @@ import { PermissionGuard } from './providers/permission.guard';
 import { PermissionsService } from './providers/permissions.service';
 import { UsersEventsListener } from './providers/users-events.listener';
 import { UsersPolicy } from './providers/users.policy';
-import { AuthService } from './services/auth.service';
-import { NotificationsService } from './services/notifications.service';
-import { RolesService } from './services/roles.service';
-import { UsersService } from './services/users.service';
+import {
+  platformServiceExports,
+  platformServiceProviders,
+} from './providers/interfaces.service';
 
 @Module({
   imports: [
@@ -44,56 +39,21 @@ import { UsersService } from './services/users.service';
   ],
   controllers: [AuthController, HomeController, LanguageController, UsersController],
   providers: [
-    AuthService,
     JwtStrategy,
     {
       provide: APP_GUARD,
       useClass: JwtAuthGuard,
     },
-    NotificationsService,
     NotificationsListener,
-    PermissionsService,
     PermissionGuard,
-    RolesService,
-    UsersService,
     UsersPolicy,
     UsersEventsListener,
     {
       provide: APP_GUARD,
       useClass: PermissionGuard,
     },
-    {
-      provide: AUTH_SERVICE,
-      useExisting: AuthService,
-    },
-    {
-      provide: NOTIFICATION_SERVICE,
-      useExisting: NotificationsService,
-    },
-    {
-      provide: PERMISSION_SERVICE,
-      useExisting: PermissionsService,
-    },
-    {
-      provide: ROLE_SERVICE,
-      useExisting: RolesService,
-    },
-    {
-      provide: USER_SERVICE,
-      useExisting: UsersService,
-    },
+    ...platformServiceProviders,
   ],
-  exports: [
-    AuthService,
-    NotificationsService,
-    PermissionsService,
-    RolesService,
-    UsersService,
-    AUTH_SERVICE,
-    NOTIFICATION_SERVICE,
-    PERMISSION_SERVICE,
-    ROLE_SERVICE,
-    USER_SERVICE,
-  ],
+  exports: platformServiceExports,
 })
 export class PlatformModule {}
